f21d1e43b938ffab52fc831d27e3778faa75d8b7
rajkiranbir09 <61343584+rajkiranbir09@users.noreply.github.com>
2020-08-16 14:18:43 +0530

    Create FlexField.java

---
+++ src/main/java/com/ivytech/unitinfoservice/commons/FlexField.java
@@ -0,0 +1,62 @@
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;attribute name="name" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "")
+public class FlexField {
+
+    @XmlAttribute(name = "name")
+    protected String name;
+    @XmlValue
+    protected String value;
+  
+
+     public String getValue() {
+		return value;
+	 }
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+    /**
+     * Gets the value of the name property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the value of the name property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+}
+
